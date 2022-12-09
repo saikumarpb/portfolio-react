f181109ec1772fd5397794e9f762376de5d4db4d
sai <saipusarla7094@gmail.com>
2022-12-10 00:07:13 +0530

    Initial Cleanup
    - Remove unwanted files.
    - Add env.sample

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,9 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link font-bold underline"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex justify-center items-center h-[100vh] bg-black text-cyan-50">
+      Let's get started
     </div>
   );
 }
