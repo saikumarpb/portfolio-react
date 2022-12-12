15b43b560b1cedff5253b04d0d98c381a3c7f177
sai <saipusarla7094@gmail.com>
2022-12-12 19:17:55 +0530

    Add basic navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import React from "react";
+import Navbar from "./features/Navbar";
 
 function App() {
   return (
-    <div className="flex justify-center items-center h-[100vh] bg-black text-cyan-50">
-      Let's get started
+    <div className="w-full">
+      <div className="h-16 w-full">
+        <Navbar />
+      </div>
+      <div className="h-[calc(100vh-64px)] text-cyan-50 relative">
+        Let's get started
+      </div>
     </div>
   );
 }
